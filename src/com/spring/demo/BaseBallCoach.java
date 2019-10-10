c4e65884bca090337cd5509c28d8b84139fe2c8a
user <user@192.168.1.4>
2019-10-10 23:20:46 +0530

    InversionOfControl(IOC)

---
+++ src/com/spring/demo/BaseBallCoach.java
@@ -0,0 +1,10 @@
+package com.spring.demo;
+
+public class BaseBallCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return "practise 30 minutes daily";
+	}
+
+}
---
+++ src/com/spring/demo/MyApp.java
@@ -0,0 +1,11 @@
+package com.spring.demo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
